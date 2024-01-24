cec238e0c3ce7aa7f7592df56ae77e32cfc34a7f
sunny14 kk <faina.private@gmail.com>
2024-01-24 08:35:17 +0200

    Add project files.

---
+++ BabushkaApp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Babushka
+{
+    class Program
+    {
+
+        public static void babushka(int BabushkaNumber)
+        {
+            Console.WriteLine("I opened babushka number "+BabushkaNumber);
+            if (BabushkaNumber == 1) // תנאי עצירה
+            {
+                Console.WriteLine("Found the smallest babushka!");
+                Console.WriteLine("I closed the smallest babushka!");
+                return;
+            }
+            babushka(BabushkaNumber - 1); // קריאה רקורסיבית
+            Console.WriteLine("I closed babushka number " + BabushkaNumber);
+        }
+
+        static void Main(string[] args)
+        {
+            int SetSize = 7;
+            babushka(SetSize);
+        }
+    }
+}
